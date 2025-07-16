f00448f22cf167444be4bc7826d4ae0d024659cf
FREITASWES <freitass.wesley@gmail.com>
2025-07-16 09:02:14 -0300

    refactor: removendo await desnecessário de creatRole Service

--- src/repositories/roleRepository.js
+++ src/repositories/roleRepository.js
@@ -2,7 +2,7 @@ const Role = require('../models/role-model');
 
 async function createRole(roleData) {
     const role = new Role(roleData);
-    return await role.save();
+    return role.save();
 }
 
 module.exports = {
