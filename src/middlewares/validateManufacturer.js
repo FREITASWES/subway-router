8eecf8733ea05555b04611e1e2c7373a0e3ace0c
FREITASWES <freitass.wesley@gmail.com>
2025-10-22 00:36:27 -0300

    feat: implement manufacturer feature with validation

---
+++ src/middlewares/validateManufacturer.js
@@ -0,0 +1,54 @@
+const yup = require('yup');
+
+const manufacturerSchema = yup.object({
+    name: yup
+        .string()
+        .required('Manufacturer name is required'),
+
+    contact_info: yup.object({
+        phone: yup
+            .string()
+            .required('Contact phone is required'),
+        email: yup
+            .string()
+            .email('invalid email format')
+            .required('Contact email is required'),
+    }),
+
+    address: yup.object({
+        street: yup
+        .string()
+        .required('Street is required'),
+        number: yup
+        .string()
+        .required('Number is required'),
+        city: yup
+        .string()
+        .required('City is required'),
+        state: yup
+        .string()
+        .required('State is required'),
+        zip_code: yup
+        .string()
+        .required('Zip code is required')
+    }),
+
+    website: yup
+    .string()
+    .url('Invalid URL format')
+    .required('Website is required')
+});
+
+async function validateManufacturer(req, res, next) {
+    try {
+        await manufacturerSchema.validate(req.body, {abortEarly: false});
+        next();
+    } catch (err) {
+        return res.status(400).json({
+            error: 'Validation error',
+            messages: err.errors
+        });
+    } 
+}
+
+module.exports = validateManufacturer;
---
+++ src/models/manufacturer-model.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const manufacturerSchema = new mongoose.Schema({
+    name: {
+        type: String, 
+        required: true,        
+    },
+    contact_info: {
+        phone: {
+            type: String, 
+            required: true,        
+        },
+    email: {
+        type: String,
+        required: true,
+    },
+},
+address: {
+    street: {
+        type: String, 
+        required: true,
+    },
+    number: {
+        type: String,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    state: {
+        type: String,
+        required: true,
+    },
+    zip_code: {
+        type: String,
+        required: true,
+    },
+},
+website: {
+    type: String,
+    required: true,
+    },
+});
+
+const Manufacturer = mongoose.model('Manufacturer', manufacturerSchema);
+
+module.exports = Manufacturer;
